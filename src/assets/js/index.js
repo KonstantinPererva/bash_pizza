e2822771330b251419f63152dd2e9c0b10b536c5
unknown <pererva.kostya@outlook.com>
2021-04-10 01:07:31 +0300

    create footer and new product csrd

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -13,23 +13,33 @@ function initCarousel(selector, options)
     }
 }
 
-initCarousel(
-    '.main-slider',
-    {
-        loop: true,
-        speed: 600,
-        autoplay: {
-            delay: 4000,
-        },
-        pagination: {
-            el: '.pagination-bullets-box',
-            type: 'bullets',
-            clickable: true
-        },
-        navigation: {
-            prevEl: '[data-button="main-slider-prev"]',
-            nextEl: '[data-button="main-slider-next"]',
-        },
-        slidesPerView: 1
-    }
-);
+let banners = document.querySelectorAll('.main-slider .swiper-slide');
+let nav = document.querySelector('.wr-navigation');
+
+
+
+if (banners.length > 1){
+    initCarousel(
+        '.main-slider',
+        {
+            loop: true,
+            speed: 600,
+            autoplay: {
+                delay: 4000,
+            },
+            pagination: {
+                el: '.pagination-bullets-box',
+                type: 'bullets',
+                clickable: true
+            },
+            navigation: {
+                prevEl: '[data-button="main-slider-prev"]',
+                nextEl: '[data-button="main-slider-next"]',
+            },
+            slidesPerView: 1
+        }
+    );
+} else {
+    nav.style.display = 'none';
+}
+
